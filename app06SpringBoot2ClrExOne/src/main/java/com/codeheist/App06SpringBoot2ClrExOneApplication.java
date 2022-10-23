d8aae727299988e2772c8e61cd08bd2bd78696ef
hrk00007 <hritikmanbattulwar@gmail.com>
2022-10-23 23:35:28 +0530

    app06= In this video we will learn about Command Line Runner. Command Line Runner executed by starter class using @SpringBootAnnotation.

---
+++ app06SpringBoot2ClrExOne/src/main/java/com/codeheist/App06SpringBoot2ClrExOneApplication.java
@@ -0,0 +1,13 @@
+package com.codeheist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App06SpringBoot2ClrExOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App06SpringBoot2ClrExOneApplication.class, args);
+	}
+
+}
