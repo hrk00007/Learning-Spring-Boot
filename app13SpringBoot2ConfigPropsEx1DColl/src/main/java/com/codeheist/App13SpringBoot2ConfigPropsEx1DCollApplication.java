98c322ed71b13210dafe21637c29b11bbbe6478a
hrk00007 <hritikmanbattulwar@gmail.com>
2022-10-30 22:25:10 +0530

    app13-> We are learing how to fecth and use application.properties with collection objects

---
+++ app13SpringBoot2ConfigPropsEx1DColl/src/main/java/com/codeheist/App13SpringBoot2ConfigPropsEx1DCollApplication.java
@@ -0,0 +1,13 @@
+package com.codeheist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App13SpringBoot2ConfigPropsEx1DCollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App13SpringBoot2ConfigPropsEx1DCollApplication.class, args);
+	}
+
+}
